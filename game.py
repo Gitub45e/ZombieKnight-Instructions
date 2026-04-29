ce274293d4047b78e413926a76c3a5ff25ba85bc
HAMILCOR000 <yublying000@gmail.com>
2026-04-29 11:46:20 -0400

    Tony

--- game.py
+++ game.py
@@ -10,49 +10,73 @@ class Game:
 
     def __init__(self, player, zombie_group, platform_group, portal_group, bullet_group, ruby_group):
         """Initialize the game"""
-        pass
-
-
+        self.player = player
+        self.zombie_group = zombie_group
+        self.platform_group = platform_group
+        self.portal_group = portal_group
+        self.bullet_group = bullet_group
+        self.ruby_group = ruby_group
+        self.zombie_group.start_new_round()
+        self.platform_group.start_new_round()
+        self.portal_group.start_new_round()
+        self.bullet_group.start_new_round()
+        self.ruby_group.start_new_round()
     def update(self):
         """Update the game"""
-        pass
-
-
+        self.zombie_group.update()
+        self.platform_group.update()
+        self.portal_group.update()
+        self.bullet_group.update()
+        self.ruby_group.update()
     def draw(self):
         """Draw the game HUD"""
-        pass
-
-
+        self.zombie_group.draw()
+        self.platform_group.draw()
+        self.portal_group.draw()
+        self.bullet_group.draw()
+        self.ruby_group.draw()
     def add_zombie(self):
         """Add a zombie to the game"""
-        pass
-
-
+        self.zombie_group.add_zombie()
     def check_collisions(self):
         """Check collisions that affect gameplay"""
-        pass
-
-
+        self.zombie_group.check_collisions()
+        self.platform_group.check_collisions()
+        self.portal_group.check_collisions()
+        self.bullet_group.check_collisions()
+        self.ruby_group.check_collisions()
     def check_round_completion(self):
         """Check if the player survived a single night"""
-        pass
-
-
+        self.zombie_group.check_round_completion()
+        self.platform_group.check_round_completion()
+        self.portal_group.check_round_completion()
+        self.bullet_group.check_round_completion()
+        self.ruby_group.check_round_completion()
     def check_game_over(self):
         """Check to see if the player lost the game"""
-        pass
-
-
+        self.zombie_group.check_game_over()
+        self.platform_group.check_game_over()
+        self.portal_group.check_game_over()
+        self.bullet_group.check_game_over()
+        self.ruby_group.check_game_over()
     def start_new_round(self):
         """Start a new night"""
-        pass
-
-
+        self.zombie_group.start_new_round()
+        self.platform_group.start_new_round()
+        self.portal_group.start_new_round()
+        self.bullet_group.start_new_round()
+        self.ruby_group.start_new_round()
     def pause_game(self, main_text, sub_text):
         """Pause the game"""
-        pass
-
-
+        self.zombie_group.pause_game(main_text, sub_text)
+        self.platform_group.pause_game(main_text, sub_text)
+        self.portal_group.pause_game(main_text, sub_text)
+        self.bullet_group.pause_game(main_text, sub_text)
+        self.ruby_group.pause_game(main_text, sub_text)
     def reset_game(self):
         """Reset the game"""
-        pass
+        self.zombie_group.reset_game()
+        self.platform_group.reset_game()
+        self.portal_group.reset_game()
+        self.bullet_group.reset_game()
+        self.ruby_group.reset_game()
--- settings.py
+++ settings.py
@@ -1,14 +1,10 @@
 import pygame
 
-# TODO: call pygame.init()
-
-# TODO: assign 1280 to WINDOW_WIDTH
-# TODO: assign 736 top WINDOW_HEIGHT
-# TODO: assign 60 to FPS
-
-# TODO: assign pygame.math.Vector2 to vector
-
-# TODO: assign pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) to display_surface
-# TODO: call pygame.display.set_caption() and pass in "Zombie Knight" as it's only argument
-
-# TODO: assign pygame.time.Clock() to clock
+pygame.init()
+WINDOW_WIDTH =1280
+WINDOW_HEIGHT =736
+FPS = 60
+vector = pygame.math.Vector2
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Zombie Knight")
+clock = pygame.time.Clock()
